cd02a0bbcc30cea8da7287df17f0f0229ec3780e
Jonas Gesthuizen <jonas.gesthuizen@destiny.be>
2026-03-18 18:03:56 +0100

    added player.py, updated main.py and constants.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pygame
 import constants
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}" )
     print(f"Screen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    # spawn point
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    
+    player_ship = Player(x, y)
+
 
     # FPS and game-timing
     clock = pygame.time.Clock()
@@ -20,6 +27,7 @@ def main():
                 return  
 
         screen.fill("black")
+        player_ship.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
