0a916ebfe63df22730380ec3e7785a8aebc00d32
Jonas Gesthuizen <jonas.gesthuizen@destiny.be>
2026-03-18 17:23:52 +0100

    updated main.py, added circleshape.py

--- main.py
+++ main.py
@@ -8,15 +8,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    # FPS and game-timing
+    clock = pygame.time.Clock()
+    dt = 0
+
     # infinite WHILE loop for game
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return  
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
